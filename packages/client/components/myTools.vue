54d6934c96c6f1f282c70a3f7532d09cb2b09ab9
Wesley Ford <wes@wesdev.pro>
2022-07-28 06:00:30 -0400

    Update myTools.vue

--- packages/client/components/myTools.vue
+++ packages/client/components/myTools.vue
@@ -1,8 +1,20 @@
 <template>
    <section class="section my-tools-container">
-      <font-awesome-icon class="has-text-info fa-4x" :icon="['fab', 'html5']" />
-      <font-awesome-icon class="has-text-info fa-4x" :icon="['fab', 'sass']" />
-      <font-awesome-icon class="has-text-info fa-4x" :icon="['fab', 'vuejs']" />
+      <font-awesome-icon
+         class="has-text-info fa-4x"
+         :icon="['fab', 'html5']"
+         style="color: rgb(227, 76, 38) !important"
+      />
+      <font-awesome-icon
+         class="has-text-info fa-4x"
+         :icon="['fab', 'sass']"
+         style="color: rgb(205, 103, 153) !important"
+      />
+      <font-awesome-icon
+         class="has-text-info fa-4x"
+         :icon="['fab', 'vuejs']"
+         style="color: rgb(66, 184, 131) !important"
+      />
       <font-awesome-icon
          class="has-text-info fa-4x"
          :icon="['fab', 'node-js']"
@@ -14,5 +26,8 @@
 <style lang="scss" scoped>
 .my-tools-container {
    display: flex;
+   svg {
+      margin: auto;
+   }
 }
 </style>
