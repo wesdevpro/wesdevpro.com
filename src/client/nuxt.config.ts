3bc27d010c05987ea76fea6eb31d06182d76e15b
Wesley Ford <wes@wesdev.pro>
2023-10-11 00:32:28 -0500

    Updated SEO

--- src/client/nuxt.config.ts
+++ src/client/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en' },
+      link: [{ rel: 'canonical', href: 'https://wesdevpro.com' }],
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
