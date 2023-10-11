7c6430dd28a11b60957214f2706fb59837a3a74d
Wesley Ford <wes@wesdev.pro>
2023-10-11 01:34:46 -0500

    Added `nav-brand` Image

--- src/client/components/NavBar.vue
+++ src/client/components/NavBar.vue
@@ -1,9 +1,43 @@
 <template>
     <div class="navbar bg-gray-500">
         <div class="navbar-start">
+            <div class="flex-1">
+                <a class="btn btn-ghost normal-case text-xl">
+                    <figure>
+                        <img :src="navLogo" class="navbar-brand" alt="wesdepro's Logo"/>
+                    </figure>
+                </a>
+            </div>
         </div>
         <div class="navbar-end">
 
         </div>
     </div>
 </template>
+
+<style>
+.navbar-brand {
+    height: ;
+}
+</style>
+
+<script lang="ts">
+import brandLogo from '~/assets/img/brand-logos/brand-logo.png'
+
+export default {
+    props: {
+        navRoutes: {
+            type: Array,
+            default: () => [{
+                title: 'Home',
+                path: '/'
+            }],
+        }
+    },
+    data() {
+        return {
+            navLogo: brandLogo
+        }
+    }
+}
+</script>
