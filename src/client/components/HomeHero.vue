c2324422d3773de7aa68fbd37ae256a6bd3a0406
Wesley Ford <wes@wesdev.pro>
2023-10-11 03:19:49 -0500

    Add More Features To Navbar (#9)
    * Update CODE_OF_CONDUCT.md
    
    * Removed Unnecessary Docs
    
    * Reorganize Image Assets
    
    * Optimized Existing Components
    
    * Added `nav-brand` Image
    
    * Added `v-for` Rendering Navigation Route Links
    
    * Added Icons And Theme Switcher
    
    * Fix `DaisyUI` Themes

---
+++ src/client/components/HomeHero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hero min-h-screen" :style="{ backgroundImage: `url(${heroImageURL})` }">
+        <div class="hero-overlay bg-accent bg-opacity-50"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md text-neutral">
+                <h1 class="mb-5 text-5xl font-bold">Welcome!</h1>
+                <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button class="btn btn-primary">Get Started</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import heroImageURL from '~/assets/img/misc/hero-image.jpg'
+
+export default
+    {
+        data() {
+            return { heroImageURL }
+        }
+    }
+</script>
--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,5 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: [require('daisyui')],
-}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          "primary": "#004571",
+          "secondary": "#96BBCA",
+          "accent": "#3B3B3B",
+          "neutral": "#ffffff",
+          "base-100": "#ffffff",
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "primary": "#004571",
+          "secondary": "#96BBCA",
+          "accent": "#3B3B3B",
+          "neutral": "#ffffff",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+};
